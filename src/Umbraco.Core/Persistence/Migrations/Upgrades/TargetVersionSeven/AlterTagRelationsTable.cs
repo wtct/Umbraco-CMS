fdefc08840860bec8005b73522cb406371cd3a90
Wojciech Tengler <wojciech.tengler@dreamlab.pl>
2020-08-26 14:27:16 +0200

    Proper new propertyTypeId column filling of cmsTagRelationship

--- src/Umbraco.Core/Persistence/Migrations/Upgrades/TargetVersionSeven/AlterTagRelationsTable.cs
+++ src/Umbraco.Core/Persistence/Migrations/Upgrades/TargetVersionSeven/AlterTagRelationsTable.cs
@@ -86,7 +86,7 @@ namespace Umbraco.Core.Persistence.Migrations.Upgrades.TargetVersionSeven
             var tagRelations = Context.Database.Fetch<TagRelationshipDto>("SELECT nodeId, tagId FROM cmsTagRelationship");
 
             //get all node id -> property type id references for nodes that are in a tag relations and for properties that are of those nodes that are of the tag data type
-            var propertyTypeIdRef = Context.Database.Fetch<PropertyTypeReferenceDto>(@"SELECT DISTINCT cmsTagRelationship.nodeId as NodeId, cmsPropertyType.id as PropertyTypeId
+            var propertyTypeIdRef = Context.Database.Fetch<PropertyTypeReferenceDto>(@"SELECT DISTINCT cmsTagRelationship.nodeId as NodeId, cmsTags.id as TagId, cmsPropertyType.id as PropertyTypeId
                 FROM cmsTags 
                 INNER JOIN cmsTagRelationship ON cmsTagRelationship.tagId = cmsTags.id
                 INNER JOIN umbracoNode ON umbracoNode.id = cmsTagRelationship.nodeId
@@ -94,15 +94,17 @@ namespace Umbraco.Core.Persistence.Migrations.Upgrades.TargetVersionSeven
                 INNER JOIN cmsContentType ON cmsContentType.nodeId = cmsContent.contentType
                 INNER JOIN cmsPropertyType ON cmsPropertyType.contentTypeId = cmsContentType.nodeId
                 INNER JOIN cmsDataType ON cmsDataType.nodeId = cmsPropertyType.dataTypeId
-                WHERE cmsDataType.controlId = '4023E540-92F5-11DD-AD8B-0800200C9A66'");
+                INNER JOIN cmsDataTypePreValues ON cmsDataTypePreValues.dataTypeNodeId = cmsDataType.nodeId
+                WHERE cmsDataType.controlId = '4023E540-92F5-11DD-AD8B-0800200C9A66'
+                    AND cmsDataTypePreValues.Alias = 'group' AND cmsTags.[Group] = cmsDataTypePreValues.[Value]");
 
             foreach (var tr in tagRelations)
             {
                 //for each tag relation we need to assign it a property type id which must exist in our references, if it doesn't it means that 
                 // someone has tag data that relates to node that is not in the cmsContent table - we'll have to delete it and log it if that is the case.
 
-                var propertyTypes = propertyTypeIdRef.Where(x => x.NodeId == tr.NodeId).ToArray();
-                if (propertyTypes.Length == 0)
+                var propertyTypeId = propertyTypeIdRef.Where(x => x.NodeId == tr.NodeId && x.TagId == tr.TagId).SingleOrDefault();
+                if (propertyTypeId == null)
                 {
                     Logger.Warn<AlterTagRelationsTable>("There was no cmsContent reference for cmsTagRelationship for nodeId "
                         + tr.NodeId +
@@ -112,23 +114,9 @@ namespace Umbraco.Core.Persistence.Migrations.Upgrades.TargetVersionSeven
                 }
                 else
                 {
-                    //update the first one found to the existing row, there might be more if there are more than one tag property assigned to the node
-                    //in that case we need to create a row.
-
-                    //update the table with the alias, the current editorAlias will contain the original id
-                    var first = propertyTypes[0];
                     Update.Table("cmsTagRelationship")
-                          .Set(new { propertyTypeId = first.PropertyTypeId })
+                          .Set(new { propertyTypeId })
                           .Where(new { nodeId = tr.NodeId, tagId = tr.TagId });
-
-                    if (propertyTypes.Length > 1)
-                    {
-                        //now we need to create rows for the other ones
-                        for (var i = 1; i < propertyTypes.Length; i++)
-                        {
-                            Insert.IntoTable("cmsTagRelationship").Row(new { nodeId = tr.NodeId, tagId = tr.TagId, propertyTypeId = propertyTypes[i].PropertyTypeId });
-                        }
-                    }
                 }
             }
         }
@@ -171,6 +159,7 @@ namespace Umbraco.Core.Persistence.Migrations.Upgrades.TargetVersionSeven
         private class PropertyTypeReferenceDto
         {
             public int NodeId { get; set; }
+            public int TagId { get; set; }
             public int PropertyTypeId { get; set; }
         }
     }
