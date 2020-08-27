600f7b0a09dc9411f0d5a5c83f916f22dc9c488b
Wojciech Tengler <wojciech.tengler@dreamlab.pl>
2020-08-27 11:19:42 +0200

    AlterTagRelationTable migration optimization by special T-SQL

--- src/Umbraco.Core/Persistence/Migrations/Upgrades/TargetVersionSeven/AlterTagRelationsTable.cs
+++ src/Umbraco.Core/Persistence/Migrations/Upgrades/TargetVersionSeven/AlterTagRelationsTable.cs
@@ -82,55 +82,37 @@ namespace Umbraco.Core.Persistence.Migrations.Upgrades.TargetVersionSeven
 
         private void Upgrade()
         {
-            //get all data from the tag relationship table
-            var tagRelations = Context.Database.Fetch<TagRelationshipDto>("SELECT nodeId, tagId FROM cmsTagRelationship");
+            Execute.Sql(@"DELETE TR
+                FROM cmsTagRelationship TR
+                WHERE NOT EXISTS (SELECT DISTINCT cmsTagRelationship.nodeId as NodeId, cmsTags.id as TagId
+                    FROM cmsTags 
+                    JOIN cmsTagRelationship ON cmsTagRelationship.tagId = cmsTags.id
+                    JOIN umbracoNode ON umbracoNode.id = cmsTagRelationship.nodeId
+                    JOIN cmsContent ON cmsContent.nodeId = umbracoNode.id
+                    JOIN cmsContentType ON cmsContentType.nodeId = cmsContent.contentType
+                    JOIN cmsPropertyType ON cmsPropertyType.contentTypeId = cmsContentType.nodeId
+                    JOIN cmsDataType ON cmsDataType.nodeId = cmsPropertyType.dataTypeId
+                    JOIN cmsDataTypePreValues ON cmsDataTypePreValues.dataTypeNodeId = cmsDataType.nodeId
+                    WHERE cmsDataType.controlId = '4023E540-92F5-11DD-AD8B-0800200C9A66' 
+					    AND cmsDataTypePreValues.Alias = 'group' AND cmsTags.[Group] = cmsDataTypePreValues.[Value]
+					    AND cmsTagRelationship.nodeId = TR.NodeId 
+					    AND cmsTags.id = TR.TagId)");
 
-            //get all node id -> property type id references for nodes that are in a tag relations and for properties that are of those nodes that are of the tag data type
-            var propertyTypeIdRef = Context.Database.Fetch<PropertyTypeReferenceDto>(@"SELECT DISTINCT cmsTagRelationship.nodeId as NodeId, cmsPropertyType.id as PropertyTypeId
-                FROM cmsTags 
-                INNER JOIN cmsTagRelationship ON cmsTagRelationship.tagId = cmsTags.id
-                INNER JOIN umbracoNode ON umbracoNode.id = cmsTagRelationship.nodeId
-                INNER JOIN cmsContent ON cmsContent.nodeId = umbracoNode.id
-                INNER JOIN cmsContentType ON cmsContentType.nodeId = cmsContent.contentType
-                INNER JOIN cmsPropertyType ON cmsPropertyType.contentTypeId = cmsContentType.nodeId
-                INNER JOIN cmsDataType ON cmsDataType.nodeId = cmsPropertyType.dataTypeId
-                WHERE cmsDataType.controlId = '4023E540-92F5-11DD-AD8B-0800200C9A66'");
-
-            foreach (var tr in tagRelations)
-            {
-                //for each tag relation we need to assign it a property type id which must exist in our references, if it doesn't it means that 
-                // someone has tag data that relates to node that is not in the cmsContent table - we'll have to delete it and log it if that is the case.
-
-                var propertyTypes = propertyTypeIdRef.Where(x => x.NodeId == tr.NodeId).ToArray();
-                if (propertyTypes.Length == 0)
-                {
-                    Logger.Warn<AlterTagRelationsTable>("There was no cmsContent reference for cmsTagRelationship for nodeId "
-                        + tr.NodeId +
-                        ". The new tag system only supports tags with references to content in the cmsContent and cmsPropertyType tables. This row will be deleted: "
-                        + string.Format("nodeId: {0}, tagId: {1}", tr.NodeId, tr.TagId));
-                    Delete.FromTable("cmsTagRelationship").Row(new { nodeId = tr.NodeId, tagId = tr.TagId });
-                }
-                else
-                {
-                    //update the first one found to the existing row, there might be more if there are more than one tag property assigned to the node
-                    //in that case we need to create a row.
-
-                    //update the table with the alias, the current editorAlias will contain the original id
-                    var first = propertyTypes[0];
-                    Update.Table("cmsTagRelationship")
-                          .Set(new { propertyTypeId = first.PropertyTypeId })
-                          .Where(new { nodeId = tr.NodeId, tagId = tr.TagId });
-
-                    if (propertyTypes.Length > 1)
-                    {
-                        //now we need to create rows for the other ones
-                        for (var i = 1; i < propertyTypes.Length; i++)
-                        {
-                            Insert.IntoTable("cmsTagRelationship").Row(new { nodeId = tr.NodeId, tagId = tr.TagId, propertyTypeId = propertyTypes[i].PropertyTypeId });
-                        }
-                    }
-                }
-            }
+            Execute.Sql(@"UPDATE TR
+                SET TR.propertyTypeId = (SELECT cmsPropertyType.id
+                    FROM cmsTags 
+                    JOIN cmsTagRelationship ON cmsTagRelationship.tagId = cmsTags.id
+                    JOIN umbracoNode ON umbracoNode.id = cmsTagRelationship.nodeId
+                    JOIN cmsContent ON cmsContent.nodeId = umbracoNode.id
+                    JOIN cmsContentType ON cmsContentType.nodeId = cmsContent.contentType
+                    JOIN cmsPropertyType ON cmsPropertyType.contentTypeId = cmsContentType.nodeId
+                    JOIN cmsDataType ON cmsDataType.nodeId = cmsPropertyType.dataTypeId
+                    JOIN cmsDataTypePreValues ON cmsDataTypePreValues.dataTypeNodeId = cmsDataType.nodeId
+                    WHERE cmsDataType.controlId = '4023E540-92F5-11DD-AD8B-0800200C9A66' 
+					    AND cmsDataTypePreValues.Alias = 'group' AND cmsTags.[Group] = cmsDataTypePreValues.[Value] 
+					    AND cmsTagRelationship.NodeId = TR.NodeId
+					    AND cmsTagRelationship.TagId = TR.TagId)
+                FROM cmsTagRelationship TR");
         }
 
         private void Final()
@@ -163,15 +145,6 @@ namespace Umbraco.Core.Persistence.Migrations.Upgrades.TargetVersionSeven
         public override void Down()
         {
             throw new DataLossException("Cannot downgrade from a version 7 database to a prior version, the database schema has already been modified");
-        }
-
-        /// <summary>
-        /// A custom class to map to so that we can linq to it easily without dynamics
-        /// </summary>
-        private class PropertyTypeReferenceDto
-        {
-            public int NodeId { get; set; }
-            public int PropertyTypeId { get; set; }
         }
     }
 }
